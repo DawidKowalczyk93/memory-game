3c666c57eae0353b4718d1fd1bc2159574a94b45
DawidKowalczyk93 <bandiwaves@gmail.com>
2024-03-06 12:41:48 +0100

    add new pictures, new font

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import SingleCard from "./components/SingleCard";
 import soundMatched from "./sound/win_success_2.wav"
 
 const cardImages = [
-    {"src": "/img/helmet-1.png", matched: false},
+    {"src": "/img/paper.png", matched: false},
     {"src": "/img/potion-1.png", matched: false},
-    {"src": "/img/ring-1.png", matched: false},
-    {"src": "/img/scroll-1.png", matched: false},
-    {"src": "/img/shield-1.png", matched: false},
-    {"src": "/img/sword-1.png", matched: false}
+    {"src": "/img/hat.png", matched: false},
+    {"src": "/img/plant.png", matched: false},
+    {"src": "/img/chest.png", matched: false},
+    {"src": "/img/potion-2.png", matched: false}
 ]
 
 function App() {
